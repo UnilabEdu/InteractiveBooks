2eb265301b074e2f388959bbead2e7a96f915293
Anano24 <ananorobakidze24@gmail.com>
2024-03-10 18:55:34 +0400

    Add pagination on main and book_lab pages

--- src/views/book_lab/routes.py
+++ src/views/book_lab/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 
 from src.models import Book
 
@@ -8,5 +8,11 @@ books_blueprint = Blueprint("books", __name__)
 
 @books_blueprint.route("/book_laboratory")
 def book_lab():
-    books = Book.query.all()
-    return render_template("book_lab/book_list.html", books=books)
+    
+    page = request.args.get('page', 1, type=int)
+    per_page = 1  # Number of products per page
+    pagination = Book.query.paginate(page=page, per_page=per_page, error_out=False)
+    products_on_page = pagination.items
+    total_pages = pagination.pages
+
+    return render_template('book_lab/book_list.html', books=pagination, total_pages=total_pages, products_on_page=products_on_page, page=page)
--- src/views/main/routes.py
+++ src/views/main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 
 from src.models import Book
 
@@ -9,8 +9,16 @@ main_blueprint = Blueprint("main", __name__)
 @main_blueprint.route("/")
 @main_blueprint.route("/home")
 def home():
-    books = Book.query.all()
-    return render_template("main/main.html", books=books)
+    # books = Book.query.all()
+    # return render_template("main/main.html", books=books)
+
+    page = request.args.get('page', 1, type=int)
+    per_page = 3  # Number of products per page
+    pagination = Book.query.paginate(page=page, per_page=per_page, error_out=False)
+    products_on_page = pagination.items
+    total_pages = pagination.pages
+
+    return render_template('main/main.html', books=pagination, total_pages=total_pages, products_on_page=products_on_page, page=page)
 
 
 
