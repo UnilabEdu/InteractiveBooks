9c415debd896132f89f5039146090211bf2d3065
Saba Dvali <s.dvali@outlook.com>
2024-07-18 17:49:12 +0400

    added pagination

--- src/views/book/routes.py
+++ src/views/book/routes.py
@@ -15,9 +15,13 @@ def book(id):
 @book_blueprint.route("/book_laboratory")
 def book_lab():
     search = request.args.get("search")
+    page_num = request.args.get("page_num")
+    print(page_num)
     if search:
         search = f"%{search}%"
         books = Book.query.filter(Book.project_name.ilike(search))
     else:
-        books = Book.query.all()
-    return render_template("book_lab/book_list.html", books=books)
+        books = Book.query.paginate(per_page=1,  error_out=True)
+
+        # books = Book.query.all()
+    return render_template("book_lab/book_list.html",page_num=page_num, books=books)
